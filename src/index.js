fb568e00b62cbe835132921c6aede281911c5b8e
dgekaa <dgeka_vip@mail.ru>
2018-09-27 22:08:58 +0300

    dich

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+var firstArr = [], 
+	secondArr = [], 
+	thirdArr = [];
+var count = 0,
+	rez = 0;
+
+    for (var i = 2; i <= base; i++){
+   		if (base%i === 0) {
+    		firstArr.push(i);
+    	}
+        while (base%i === 0){
+            base = base/i;
+            count++;
+        }
+        if (count > 0) {
+            secondArr.push(count);
+        }
+        count = 0;
+    }
+    var y = 2;
+    for (var i = 0; i < firstArr.length; i++){
+            var x = firstArr[i];
+        while (number/x > 1){
+            rez += Math.floor(number/x);
+            x = Math.pow(firstArr[i], y);
+            y++;
+        }
+        thirdArr.push(rez);
+        rez = 0;
+        y = 2;
+    }
+    for (var i = 0; i < thirdArr.length; i++){
+        thirdArr[i] = Math.floor(thirdArr[i]/secondArr[i]);
+    }
+    thirdArr.sort(function(a, b){return a-b;});
+    return thirdArr[0];
 }
